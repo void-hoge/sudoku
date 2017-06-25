b3cfeb6a8d3ad374a4b597c45e02944f2aa1e480
elh <reo.retriever@gmail.com>
2017-06-26 01:46:15 +0900

    Alter QTableView to QTableWidget
    It needs to use wdget. Not view. And set the size 16 both its row and column.

--- gui/guiapp/mainwindow.cpp
+++ gui/guiapp/mainwindow.cpp
@@ -1,14 +1,18 @@
+#include <qtablewidget.h>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+namespace {
+    constexpr int tableSize = 16;
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    QTableWidget* table = ui->tableWidget;
+    table->setRowCount(tableSize);
+    table->setColumnCount(tableSize);
+}
+
+MainWindow::~MainWindow() {
     delete ui;
 }
