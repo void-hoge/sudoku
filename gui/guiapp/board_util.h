4af77f17bb9ca20870e7680f217d25af1b06b6b6
Unknown <reo.retriever@gmail.com>
2017-08-16 23:45:59 +0900

    Change namespace of Board class

--- gui/guiapp/board_util.h
+++ gui/guiapp/board_util.h
@@ -8,7 +8,7 @@
 using board_expression = std::array<int, all_cells_count>;
 
 // reinterpret Board data to unified type (board_expression)
-board_expression compile(v16::Board& b);
-board_expression compile(v16::Board&& b);
+board_expression compile(b16::Board& b);
+board_expression compile(b16::Board&& b);
 
 #endif // BOARD_FACADE_H
