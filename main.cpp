41bf0b77df1d907c52f6d4efd7aa8333624aa237
void hoge() <coilgunrailgun@gmail.com>
2017-08-16 16:14:33 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-//g++ main.cpp 16x16/Solver/Solver.cpp 16x16/Board/Board.cpp 9x9/Solver/Solver.cpp 9x9/Board/Board.cpp
-#include "16x16/Solver/Solver.hpp"
-#include "9x9/Solver/Solver.hpp"
+//g++ main.cpp 16x16/Solver/Solver16.cpp 16x16/Board/Board16.cpp 9x9/Solver/Solver16.cpp 9x9/Board/Board16.cpp
+#include "16x16/Solver/Solver16.hpp"
+#include "9x9/Solver/Solver16.hpp"
 
 int main(int argc, char const *argv[]) {
 	v9::Solver nine;
